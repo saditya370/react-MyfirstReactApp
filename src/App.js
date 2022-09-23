21d447b3a0358db34d5c2d36dae0fec3bf1ee073
Aditya Singh <saditya370@gmail.com>
2022-09-23 14:38:02 +0530

    Added dark mode funcitnality to whole website

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-import About from './components/About';
-// import TextForm from './components/TextForm';
+// import About from './components/About';
+import TextForm from './components/TextForm';
+import { useState } from 'react';
 
 function App() {
+  const [mode , setMode] = useState('light')
+
+  const toggleMode =() =>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
+
+
   return (
     <>
-    
-<Navbar title="TextUtils" />
-<div className="container">
+          
+      <Navbar title="TextUtils" mode = {mode} toggleMode = {toggleMode} />
+      <div className="container">
 
-{/* <TextForm heading ="Enter the text to analyze below"/> */}
-<About/>
-</div>
+      <TextForm heading ="Enter the text to analyze below" mode={mode}/>
+      {/* <About/> */}
+      </div>
 
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -32,31 +32,34 @@ export default function TextForm(props) {
         setText(event.target.value)
 
     }
-    const [text, setText] = useState("Enter text here");
+    const [text, setText] = useState("");
     
-  return (
-    <>    <div>
+    return (
+        <>   
+     <div style ={{color: props.mode==='dark'?'white':'#042743'}}>
 
-            <div class="mb-3">
-                <h1>{props.heading} - </h1>
+            <div className="mb-3">
+                <h1 >{props.heading} - </h1>
+        {console.log(props.mode)}
             
-            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" style = {{backgroundColor: props.mode=== 'dark'?'grey':'white'} } rows="8"></textarea>
           
             </div>
 
             <button  className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Upper Case</button>
-            <button  className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lower Case</button>
+            <button  className="btn btn-primary mx-1" onClick= {handleLoClick}>Convert to Lower Case</button>
             <button  className="btn btn-primary mx-1" onClick={handle}>Reverse </button>
 
             
     </div>
-    <div className="container my-3" >
+    
+    <div className="container my-3" style ={{color: props.mode ==='light'?('#042743'):('white')}} >
         <h1>your Text summary</h1>
         <p>No. of Words:{text.split(" ").length}  ||  No. of Characters: {text.length} </p>
 
         <p>{ 0.008*text.split(" ").length} Minutes Read</p>
         <h3>Preview</h3>
-        <p>{text}</p>
+        <p>{text.length === 0?"Enter text to preview":text}</p>
     </div>
     </>
 
